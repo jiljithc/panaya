19de17b021465038efaa5c07449003a458eadd55
jiljithc <jiljithsruthi@gmail.com>
2026-06-19 11:24:19 +0530

    first commit

---
+++ src/main/java/com/example/panaya/PanayaApplication.java
@@ -0,0 +1,13 @@
+package com.example.panaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanayaApplication.class, args);
+	}
+
+}
